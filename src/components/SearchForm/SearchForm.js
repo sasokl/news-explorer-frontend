fe79f6e0e8443f6c51e66e5b7aed6d766d7b5533
Alexander Blaus <devsasokl@gmail.com>
2022-07-06 00:39:58 +0300

    media query 768 added

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -22,32 +22,33 @@ function SearchForm({onSubmit}) {
 
   return (
     <section className='search-form'>
-      <h1 className="search-form__title">What's going on in the world?</h1>
-      <p className="search-form__subtitle">Find the latest news on any topic and save them in your personal account.</p>
-      <form
-        className='search-form__form-elem'
-        onSubmit={()=>{}}>
-        <input
-          type='text'
-          name='search-field'
-          id='search-input'
-          className='search-form__input'
-          value={searchText || ''}
-          onChange={handleSearchTextChange}
-          placeholder='Enter topic'
-        />
-        <button
-          onClick={handleSubmit}
-          className="search-form__button"
-          onMouseDown={handleButtonMousedown}
-          onMouseUp={handleButtonMouseup}
-          onMouseLeave={handleButtonMouseup}
-          type="submit"
-        >
-          Search
-        </button>
-      </form>
-
+      <div className="search-form__container">
+        <h1 className="search-form__title">What's going on in the world?</h1>
+        <p className="search-form__subtitle">Find the latest news on any topic and save them in your personal account.</p>
+        <form
+          className='search-form__form-elem'
+          onSubmit={()=>{}}>
+          <input
+            type='text'
+            name='search-field'
+            id='search-input'
+            className='search-form__input'
+            value={searchText || ''}
+            onChange={handleSearchTextChange}
+            placeholder='Enter topic'
+          />
+          <button
+            onClick={handleSubmit}
+            className="search-form__button"
+            onMouseDown={handleButtonMousedown}
+            onMouseUp={handleButtonMouseup}
+            onMouseLeave={handleButtonMouseup}
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
